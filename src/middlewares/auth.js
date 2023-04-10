72fc26596f3168fd136b2366fe71d34e1ab17109
Oustaa <otailaba98@gmail.com>
2023-04-10 11:20:52 +0000

    loging recieved headers

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -7,6 +7,8 @@ const JWT_SECRET = process.env.ACCESS_TOKEN_SECRET;
 async function authorization(req, res, next) {
   const token = req.cookies.access_token || req.headers["authorization"];
 
+  console.log(req.headers["authorization"]);
+
   jwt.verify(token, JWT_SECRET, function (err, user) {
     if (err || !user)
       return res
